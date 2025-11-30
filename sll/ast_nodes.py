82bcd8004bda8c5572efed1bfcf518f9dc75c46f
makezh <max.kezhaev@gmail.com>
2025-12-01 01:10:58 +0300

    Добавил Типы и сигнатуры функции

--- sll/ast_nodes.py
+++ sll/ast_nodes.py
@@ -1,7 +1,8 @@
-from dataclasses import dataclass
+from dataclasses import dataclass, field
 from typing import List
 
 
+# --- Выражения ---
 class Expr:
     """
     Базовый класс для всего, что может быть выражением
@@ -76,8 +77,47 @@ class Let(Expr):
 
     def __str__(self):
         return f"let {self.var_name} = {self.val} in {self.body}"
+# --- Конец Выражений ---
 
 
+# --- Типы, Сигнатуры и Определения ---
+@dataclass
+class TypeExpr:
+    """Представление типа, например [Nat] или [List [Nat]]"""
+    name: str
+    params: List['TypeExpr'] = field(default_factory=list)
+
+    def __str__(self):
+        if not self.params:
+            return f"[{self.name}]"
+        return f"[{self.name} {' '.join(str(p) for p in self.params)}]"
+
+
+@dataclass
+class ConstrDef:
+    """Определение конструктора: Z или S [Nat]"""
+    name: str
+    arg_types: List[TypeExpr]
+
+
+@dataclass
+class TypeDef:
+    """Определение типа: type [Nat] : Z | S [Nat]."""
+    name: str
+    params: List[str]  # имена типовых переменных, например ['a'] для [List a]
+    constructors: List[ConstrDef]
+
+
+@dataclass
+class FunSig:
+    """Сигнатура функции: (add [Nat] [Nat]) -> [Nat]"""
+    name: str
+    arg_types: List[TypeExpr]
+    ret_type: TypeExpr
+# --- Конец Типов, Сигнатур и Определений ---
+
+
+# --- Правила и Программы ---
 # Описание паттерна (то, что слева в уравнении)
 # Например: (append [Cons x xs] ys)
 @dataclass
@@ -106,11 +146,6 @@ class Rule:
         return f"{self.pattern} -> {self.body}"
 
 
-@dataclass
-class TypeDef:
-    name: str
-    constrs: List[str]  # Список конструкторов
-
 
 # Описание всей программы
 @dataclass
@@ -118,7 +153,19 @@ class Program:
     """
     Вся программа: набор уравнений
     """
-    rules: List[Rule]
+    rules: List[Rule]  # Список уравнений
+    types: List[TypeDef]  # Список определений типов
+    signatures: List[FunSig]   # Список сигнатур функций
 
     def __str__(self):
-        return "\n".join(str(rule) + ";" for rule in self.rules)
+        # return "\n".join(str(rule) + ";" for rule in self.rules)
+        res = ""
+        for t in self.types:
+            constrs = " | ".join(f"{c.name} {' '.join(str(a) for a in c.arg_types)}" for c in t.constructors)
+            res += f"type [{t.name} {' '.join(t.params)}] : {constrs} .\n"
+        res += "\n"
+        for s in self.signatures:
+            args = " ".join(str(a) for a in s.arg_types)
+            res += f"fun ({s.name} {args}) -> {s.ret_type} : ...\n"
+        return res + "\n" + "\n".join(str(r) + ";" for r in self.rules)
+# --- Конец Правил и Программ ---
